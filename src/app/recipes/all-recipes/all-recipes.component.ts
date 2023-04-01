c7c7629e66c5cdef923a3ecdaea404b346433573
Chris Stinson <cistinson@lcmail.lcsc.edu>
2023-04-01 13:10:14 -0700

    Add filter options for recipe categories Add category tags to recipe cards

--- src/app/recipes/all-recipes/all-recipes.component.ts
+++ src/app/recipes/all-recipes/all-recipes.component.ts
@@ -12,12 +12,23 @@ export class AllRecipesComponent {
 
   recipes: Recipe[] = [];
 
+  recipeCategories: any [];
+
   constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
     setTimeout(() => {
       this.recipes = this.recipeService.getRecipes();
+      let fullCategoryList = this.recipes.map(el => {
+        return el.tags;
+      })
+      this.recipeCategories = [...new Set(fullCategoryList.flat(1))];
     }, 20);
+  }
+
+
+  filterRecipes(category: string){
+    alert(category);
   }
 
 }
