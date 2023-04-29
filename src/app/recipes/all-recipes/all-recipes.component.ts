f3e411534779155dff2e554bb65844d80467db8e
Chris Stinson <cistinson@lcmail.lcsc.edu>
2023-04-28 20:17:24 -0700

    Add new dropdown filters

--- src/app/recipes/all-recipes/all-recipes.component.ts
+++ src/app/recipes/all-recipes/all-recipes.component.ts
@@ -16,10 +16,21 @@ export class AllRecipesComponent {
 
   allRecipes: Recipe[] = [];
   selectedRecipes: Recipe[] = [];
+
   expandButtonSetting: boolean = false;
   expandButtonText: string = "Show more";
+  searchInput: string;
 
-  recipeCategories: any [];
+  recipeCategories: string[];
+  recipeCookTimes: number[];
+  recipeServingSizes: number[];
+  recipeResults: Recipe[] = [];
+
+  filterLabel: string = "Filter";
+  filtersShowing: boolean = false;
+
+  recipesFiltered: boolean = false;
+
 
   constructor(private recipeService: RecipeService) {}
 
@@ -31,7 +42,21 @@ export class AllRecipesComponent {
         return el.tags;
       })
       this.recipeCategories = [...new Set(fullCategoryList.flat(1).sort())];
-      this.allCheckbox.nativeElement.checked = true;
+
+      let fullCookTImes =  this.allRecipes.map(el => {
+        return el.cookTime;
+      })
+      this.recipeCookTimes = [...new Set(fullCookTImes.flat(1).sort())];
+
+      let fullServingSizes =  this.allRecipes.map(el => {
+        return el.servingSize;
+      })
+      this.recipeServingSizes = [...new Set(fullServingSizes.flat(1).sort())];
+
+      console.log(this.recipeCategories, this.recipeCookTimes, this.recipeServingSizes);
+
+      //this.allCheckbox.nativeElement.checked = true;
+      this.recipeResults = this.allRecipes;
     }, 100);
   }
 
@@ -41,11 +66,48 @@ export class AllRecipesComponent {
     this.allCheckbox.nativeElement.checked = true;
   }
 
+  updateResults(){
+    this.selectedRecipes = this.allRecipes.filter(recipe => recipe.name.toLowerCase().includes(this.searchInput.toLowerCase()));
+  }
+
+  clearSearchInput(){
+    this.searchInput = '';
+    this.updateResults();
+  }
+
+  showFilters(){
+    this.filtersShowing = !this.filtersShowing;
+    if(this.filtersShowing){
+      this.filterLabel = "Hide"
+    } else {
+      this.filterLabel = "Filter"
+    }
+  }
+
   filterRecipes(category: string){
     this.uncheckAll();
     this.selectedRecipes = this.allRecipes.filter(recipe => recipe.tags.includes(category));
   }
 
+  filterByCategory(selectedFilter:any){
+    selectedFilter = selectedFilter.target.value;
+    this.selectedRecipes = this.allRecipes.filter(recipe => recipe.tags.includes(selectedFilter));
+    this.recipesFiltered = true;
+  }
+
+  resetFilters(){
+    const filters = Array.from(document.getElementsByClassName('filterSelects')) as HTMLSelectElement[];
+
+    filters.forEach((select: HTMLSelectElement) => {
+      select.selectedIndex = 0;
+    });
+
+    this.selectedRecipes = this.allRecipes;
+    this.recipesFiltered = false;
+  }
+
+
+  //Old TS for left-side filters
   uncheckAll(){
     this.checkboxes.forEach((element) => {
       element.nativeElement.checked = false;
