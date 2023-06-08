8c88c15f55ccfd3e7de4d2fc27c6df89632fa9cb
Chris Stinson <cistinson@lcmail.lcsc.edu>
2023-06-07 23:48:54 -0700

    Corrected ingredient pipe to avoid leading zeros Added new markers to <ol> for recipe steps

--- src/app/shared/pipes/ingredient-amount-converter.pipe.ts
+++ src/app/shared/pipes/ingredient-amount-converter.pipe.ts
@@ -10,6 +10,11 @@ export class IngredientAmountConverterPipe implements PipeTransform{
     ".5": " 1/2",
     ".66": " 2/3",
     ".75": " 3/4",
+    "0.25": "1/4",
+    "0.33": "1/3",
+    "0.5": "1/2",
+    "0.66": "2/3",
+    "0.75": "3/4"
   }
 
   transform(incomingQuantity: number, doubled: boolean) {
@@ -18,7 +23,7 @@ export class IngredientAmountConverterPipe implements PipeTransform{
       incomingQuantity = incomingQuantity * 2;
     }
 
-    let stringQuantity = incomingQuantity.toString().replace(/.25|.33|.5|.66|.75/g, matched => this.quantityAdjustments[matched]);
+    let stringQuantity = incomingQuantity.toString().replace(/0.25|0.33|0.5|0.66|0.75|.25|.33|.5|.66|.75/, matched => this.quantityAdjustments[matched]);
 
     return stringQuantity;
   }
