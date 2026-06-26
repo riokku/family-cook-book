87b8ed748d164177d8cdd0f48820c319d287c196
Chris Stinson <chrisistinson@gmail.com>
2026-06-25 22:40:11 -0700

    Add new intro

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss']
 })
-export class IntroComponent {
+export class IntroComponent implements OnInit, OnDestroy {
 
+  ngOnInit(): void {
+    document.body.style.overflow = 'hidden';
+  }
+
+  ngOnDestroy(): void {
+    document.body.style.overflow = '';
+  }
 }
